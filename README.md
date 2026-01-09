4d4972459d65fd9369377e725c020a1d8583b31e
gymynnym <gymynnym@gmail.com>
2026-01-09 15:51:09 +0900

    translate: ja

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # learn-webauthn
 
-[Passkey로 로그인: 내 비밀은 내가 관리하기](https://loghub.me/articles/gymynnym/passkey%EB%A1%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EB%82%B4-%EB%B9%84%EB%B0%80%EC%9D%80-%EB%82%B4%EA%B0%80-%EA%B4%80%EB%A6%AC%ED%95%98%EA%B8%B0) 아티클의 예제 레포지토리입니다.
+[パスキーでログイン：自分のシークレットは自分で管理する with Go](https://zenn.dev/gymynnym/articles/738baa7f6b5669) 記事のリポジトリです.
 
-Go + Gin으로 작성되었습니다.
+Go + Ginで実装しています.
--- main.go
+++ main.go
@@ -5,22 +5,22 @@ import (
 	"github.com/go-webauthn/webauthn/webauthn"
 )
 
-const ( // Gin 서버 설정
+const ( // Gin サーバー設定
 	HOSTNAME = "localhost"
 	PORT     = ":8080"
 )
 
 var (
 	userDB                 = map[string]*User{}                 // in-memory DB (username -> User)
-	registrationCacheStore = map[string]*RegistrationCache{}    // in-memory registration 임시 저장소 (uesrname -> RegistrationCache)
-	loginSessionStore      = map[string]*webauthn.SessionData{} // in-memory login 임시 저장소(username -> SessionData)
+	registrationCacheStore = map[string]*RegistrationCache{}    // in-memory registration キャッシュストア (username -> RegistrationCache)
+	loginSessionStore      = map[string]*webauthn.SessionData{} // in-memory login キャッシュストア (username -> SessionData)
 	webAuthn               *webauthn.WebAuthn
 )
 
-func initWebAuthn() { // WebAuthn 객체 초기화
+func initWebAuthn() { // WebAuthn 初期化
 	var err error
 	webAuthn, err = webauthn.New(&webauthn.Config{
-		// Relying Party 설정
+		// Relying Party 情報設定
 		RPDisplayName: "Example App",
 		RPID:          HOSTNAME,
 		RPOrigins:     []string{"http://" + HOSTNAME + PORT},
@@ -33,26 +33,26 @@ func initWebAuthn() { // WebAuthn 객체 초기화
 func beginRegistration(c *gin.Context) {
 	username := c.Param("username")
 
-	// user 존재 여부 확인
+	// ユーザー存在確認
 	if _, exists := userDB[username]; exists {
 		c.JSON(409, gin.H{"error": "User already exists"})
 		return
 	}
 
-	// 사용자 객체 생성
+	// ユーザー生成
 	user := User{
 		ID:   uint64(len(userDB) + 1),
 		Name: username,
 	}
 
-	// 등록#2: 챌린지 및 옵션 생성
+	// 登録#2: チャレンジとオプション生成
 	options, sessionData, err := webAuthn.BeginRegistration(&user)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to begin registration"})
 		return
 	}
 
-	// 임시 저장소에 세션 데이터 및 사용자 정보 저장
+	// キャッシュストアにセッションデータとユーザー情報を保存
 	registrationCacheStore[username] = &RegistrationCache{
 		SessionData: *sessionData,
 		PendingUser: &user,
@@ -63,25 +63,25 @@ func beginRegistration(c *gin.Context) {
 func finishRegistration(c *gin.Context) {
 	username := c.Param("username")
 
-	// 세션 데이터 및 사용자 정보 가져오기
+	// セッションデータとユーザー情報取得
 	cache, exists := registrationCacheStore[username]
 	if !exists {
 		c.JSON(400, gin.H{"error": "No session data found"})
 		return
 	}
 
-	// 등록#6: 검증
+	// 登録#6: 検証
 	credential, err := webAuthn.FinishRegistration(cache.PendingUser, cache.SessionData, c.Request)
 	if err != nil {
 		c.JSON(400, gin.H{"error": "Failed to finish registration"})
 		return
 	}
 
-	// 등록#6: 저장
+	// 登録#6: 保存
 	user := cache.PendingUser
-	user.Credentials = append(user.Credentials, *credential) // 새로운 자격 증명 추가
-	userDB[username] = user                                  // 사용자 DB에 저장
-	delete(registrationCacheStore, username)                 // 임시 저장소 정리
+	user.Credentials = append(user.Credentials, *credential) // 新しい資格情報を追加
+	userDB[username] = user                                  // ユーザーDBに保存
+	delete(registrationCacheStore, username)                 // キャッシュストアから削除
 
 	c.JSON(200, gin.H{"status": "Registration successful"})
 }
@@ -89,14 +89,14 @@ func finishRegistration(c *gin.Context) {
 func beginLogin(c *gin.Context) {
 	username := c.Param("username")
 
-	// user 가져오기 및 존재 여부 확인
+	// ユーザー取得と存在確認
 	user, exists := userDB[username]
 	if !exists {
 		c.JSON(404, gin.H{"error": "User not found"})
 		return
 	}
 
-	// 인증#2: 챌린지 생성
+	// 認証#2: チャレンジとオプション生成
 	options, sessionData, err := webAuthn.BeginLogin(user)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to begin login"})
@@ -110,29 +110,29 @@ func beginLogin(c *gin.Context) {
 func finishLogin(c *gin.Context) {
 	username := c.Param("username")
 
-	// user 가져오기 및 존재 여부 확인
+	// ユーザー取得と存在確認
 	user, exists := userDB[username]
 	if !exists {
 		c.JSON(404, gin.H{"error": "User not found"})
 		return
 	}
 
-	// 임시 저장소에서 세션 데이터 가져오기
+	// キャッシュストアからセッションデータ取得
 	sessionData, exists := loginSessionStore[username]
 	if !exists {
 		c.JSON(400, gin.H{"error": "No session data found"})
 		return
 	}
 
-	// #인증#6: 검증
+	// #認証#6: 検証
 	credential, err := webAuthn.FinishLogin(user, *sessionData, c.Request)
 	if err != nil {
 		c.JSON(400, gin.H{"error": "Failed to finish login"})
 		return
 	}
-	_ = credential // TODO: Credential을 활용한 세션 또는 토큰 발급
+	_ = credential // TODO: Credentialを使ったセッションまたはトークン生成
 
-	delete(loginSessionStore, username) // 임시 저장소 정리
+	delete(loginSessionStore, username) // キャッシュストアから削除
 
 	c.JSON(200, gin.H{"status": "Login successful"})
 }
